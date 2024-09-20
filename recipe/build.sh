855ae6b27ba749bfbb45204206e82ad2a65a6c2b
Tom Jakubowski <tom@crystae.net>
2024-09-20 10:07:16 -0700

    hooray

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,12 +12,14 @@ export CARGO_FEATURE_EXTERNAL_CPP=1
 export CARGO_FEATURE_EXTERNAL_PROTO=1
 
 # patch package.json
+# playwright browsers can't be installed, and aren't needed anyway
 pnpm pkg set scripts.postinstall:playwright="echo no-postinstall:playwright"
+# vscode isn't needed
 pnpm pkg set scripts.postinstall:vscode="echo no-postinstall:vscode"
 
 # remove lint from workspace - parsing lint's Cargo.toml file requires nightly
 # features, and we are building with a stable toolchain
-sed -e '|.*rust\/lint.*/d' -i'.backup' Cargo.toml
+sed -e '/.*rust\/lint.*/d' -i'.backup' Cargo.toml
 
 pnpm install --filter !@finos/perspective-bench
 # XXX(tom): TODO: unset cargo feature so as to not use ABI3
