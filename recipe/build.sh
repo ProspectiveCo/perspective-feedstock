e012cf667f79696353c4daf769d86a83dc57abb3
Tom Jakubowski <tom@crystae.net>
2024-09-19 18:23:09 -0700

    progress

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,26 @@
-export PACKAGE=
+#!/bin/bash
+set -e
 
-pnpm install
-# TODO: unset cargo feature to not use ABI3
+# XXX(tom): don't understand why it's needed to set PKG_CONFIG_PATH.
+# this directory is already present in pkg-config's config path as reported by
+# `pkg-config --variable pc_path pkg-config` in a --debug build environment
+export PKG_CONFIG_PATH=$BUILD_PREFIX/lib/pkgconfig/:$PKG_CONFIG_PATH
+export PACKAGE=perspective-python
+export PSP_BUILD_WHEEL=1
+
+export CARGO_FEATURE_EXTERNAL_CPP=1
+export CARGO_FEATURE_EXTERNAL_PROTO=1
+
+# patch package.json
+pnpm pkg set scripts.postinstall:playwright="echo no-postinstall:playwright"
+pnpm pkg set scripts.postinstall:vscode="echo no-postinstall:vscode"
+
+# remove lint from workspace - parsing lint's Cargo.toml file requires nightly
+# features, and we are building with a stable toolchain
+sed -e '|.*rust\/lint.*/d' -i'.backup' Cargo.toml
+
+pnpm install --filter !@finos/perspective-bench
+# XXX(tom): TODO: unset cargo feature so as to not use ABI3
 pnpm run build
 
 # export CARGO_FEATURE_EXTERNAL_CPP=1
