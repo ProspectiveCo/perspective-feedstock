bf66ef7e70aae7dc38711a71fdbdb284359fe385
Tom Jakubowski <tom@crystae.net>
2024-09-19 17:07:12 -0700

    now cooking

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,17 @@
-export CARGO_FEATURE_EXTERNAL_CPP=1
-export PSP_ROOT_DIR=$SRC_DIR/perspective-cpp
-cd perspective_python-${PKG_VERSION}
-cp -r ../perspective_python-${PKG_VERSION}.data .
-cd rust/perspective-client
-export CARGO_FEATURE_EXTERNAL_PROTO=1
-cargo build
-cd ../../
-unset CARGO_FEATURE_EXTERNAL_PROTO
-${PYTHON} -m pip install . -vv
-${PYTHON} ${RECIPE_DIR}/copy.py
+export PACKAGE=
+
+pnpm install
+# TODO: unset cargo feature to not use ABI3
+pnpm run build
+
+# export CARGO_FEATURE_EXTERNAL_CPP=1
+# export PSP_ROOT_DIR=$SRC_DIR/perspective-cpp
+# cd perspective_python-${PKG_VERSION}
+# cp -r ../perspective_python-${PKG_VERSION}.data .
+# cd rust/perspective-client
+# export CARGO_FEATURE_EXTERNAL_PROTO=1
+# cargo build
+# cd ../../
+# unset CARGO_FEATURE_EXTERNAL_PROTO
+# ${PYTHON} -m pip install . -vv
+# ${PYTHON} ${RECIPE_DIR}/copy.py
