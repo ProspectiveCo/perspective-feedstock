49732491e0ccaee7a623575b4a49cbb05fefdc1c
Tom Jakubowski <tom@crystae.net>
2024-09-25 02:41:16 +0000

    WIP, almost working, just failed LICENSE check

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,9 @@ set -e
 
 # XXX(tom): don't understand why it's needed to set PKG_CONFIG_PATH.
 # this directory is already present in pkg-config's config path as reported by
-# `pkg-config --variable pc_path pkg-config` in a --debug build environment
+# `pkg-config --variable pc_path pkg-config` in a --debug build environment.
+# and yet `pkg-config <some-dependency>` fails
 export PKG_CONFIG_PATH=$BUILD_PREFIX/lib/pkgconfig/:$PKG_CONFIG_PATH
-export PACKAGE=perspective-python
-export PSP_BUILD_WHEEL=1
 
 case "${target_platform}" in
   linux-aarch64|osx-arm64)
@@ -18,20 +17,26 @@ case "${target_platform}" in
 esac
 export PSP_ARCH=$arch
 
-# patch package.json to remove postinstall scripts
-# playwright browsers can't be installed, and aren't needed anyway
-pnpm pkg set scripts.postinstall:playwright="echo no-postinstall:playwright"
-# vscode + emsdk aren't needed
-pnpm pkg set scripts.postinstall:vscode="echo no-postinstall:vscode"
-pnpm pkg set scripts.postinstall:emsdk="echo no-postinstall:emsdk"
+pnpm install --filter '@finos/perspective-python'
 
 # Boost!
 # Work around boost's inability to detect cxx compiler from $CXX.
-# In short: conda provides the compiler as CXX in the environment.
-# It does not, however, provide a `g++` or `clang++` on the PATH.
-# Boost's bootstrap script looks for a suitably-named compiler on the PATH to
-# determine its toolchain, so we have to create those binaries
-# Note: assumes GNU mktemp
+# In short: conda provides a compiler binary (compiler('cxx')) on the PATH and
+# sets CXX in the environment to point to it. On Linux, the compiler (basename
+# $CXX) is called something like x86_64-conda-linux-gnu-g++, and not g++.  This
+# conflicts with Boost's bootstrap script, which looks up g++ on the PATH when
+# toolchain=gcc is set.
+# So on Linux, we have to create a g++ binary on the PATH which point to $CXX.
+# NOTE: boost-feedstock does this more rigourously, writing out a
+# site-config.jam file and also patching the boost source to give more control
+# over the toolchain.  See build.sh and patches/ in
+# https://github.com/conda-forge/boost-feedstock/tree/91de9469b5564fd816e4298d217990481c280393/recipe
+#
+# Would be great if we could just use libboost-headers as a host dependency,
+# and not need to build Boost from source and statically link to it.  Thought we
+# needed to link Boost for arrow reasons, but arrow-cpp recipe only depends on libboost-headers
+# so maybe we can too somehow.
+# Note: assumes GNU mktemp, available from coreutils package
 psp_bin=$(mktemp -d -t 'psp-bin-XXXXXX')
 case "${cxx_compiler}" in
   gxx)
@@ -42,32 +47,10 @@ case "${cxx_compiler}" in
   ;;
 esac
 
+ORIG_PATH=$PATH
 export PATH=$psp_bin:$PATH
 ln -s $CXX $psp_bin/cxx
-# ln -s $CXX $psp_bin/$cxx_compiler
 ln -s $CXX $psp_bin/$cxx_compiler_plus
-
-# XXX(tom): workaround maturin using wrong python??
-#
-echo "$$PYTHON is: $PYTHON"
-
-# XXX(tom): move me into a patch
-# remove lint from workspace - parsing lint's Cargo.toml file requires nightly
-# features, and we are building with a stable toolchain
-sed -e '/.*rust\/lint.*/d' -i'.backup' Cargo.toml
-
-pnpm install --filter '!@finos/perspective-bench'
-
-# XXX(tom): workaround maturin using wrong python??
-#
-echo "$$PYTHON is: $PYTHON"
-
-# XXX(tom): move me into a patch
-# remove lint from workspace - parsing lint's Cargo.toml file requires nightly
-# features, and we are building with a stable toolchain
-sed -e '/.*rust\/lint.*/d' -i'.backup' Cargo.toml
-
-pnpm install --filter '!@finos/perspective-bench'
 
 # Install boost:
 # This configures both the install directory for install_tools.mjs,
@@ -75,10 +58,19 @@ pnpm install --filter '!@finos/perspective-bench'
 # Note: assumes GNU mktemp
 export Boost_ROOT=$(mktemp -d -t 'psp-boost-root-XXXXXX')
 node tools/perspective-scripts/install_tools.mjs
+
+PATH=$ORIG_PATH
+
+# Run perspective build
+export PACKAGE=perspective-python
+export PSP_BUILD_WHEEL=1
 pnpm run build
 
+# Install wheel to site-packages ($SP_DIR), wherefrom Conda assembles the .conda package contents
 $PYTHON -m pip install rust/target/wheels/perspective_python*.whl -vv
 
+
+# old recipe:
 # export CARGO_FEATURE_EXTERNAL_CPP=1
 # export PSP_ROOT_DIR=$SRC_DIR/perspective-cpp
 # cd perspective_python-${PKG_VERSION}
