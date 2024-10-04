061bf68a078ccc13abfa2d80d605c9b66d704b70
Tom "Azure VM" Jakubowski <tom@prospective.dev>
2024-10-04 05:42:48 +0000

    worked locally
    seems the operative fix was the correction to shared_boost.diff to not
    change the order that find_package(Arrow) and find_package(Boost) are
    called on WIN32
    
    some stuff to undo here, probably:
    - boost-devel back to boost-headers, tried this to see if using
      boost's native FindBoostConfig.cmake was any better.   didn't help
    - revisit fix_windows.diff

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -30,15 +30,15 @@ set PSP_BUILD_WHEEL=1
 set PSP_ARCH=x86_64
 set PACKAGE=perspective-python
 set "PSP_ROOT_DIR=%SRC_DIR%"
-@REM If your build is hosted on a different drive, like at C:\bld, you may need to adjust the
-@REM CARGO_TARGET_DIR value to use the same drive, or else you may encounter an error like this:
+@REM If your build is hosted on a non-D: drive, like at C:\bld, you may need to adjust the
+@REM CARGO_TARGET_DIR value to use the same drive as the build, or else you may encounter an error like this:
 @REM Error: Os { code: 17, kind: CrossesDevices, message: "The system cannot move the file to a different disk drive." }
 set "CARGO_TARGET_DIR=D:\psp-rust"
 call pnpm install --filter "@finos/perspective-python"
 call pnpm run build
 if errorlevel 1 exit /b 1
 
-$PYTHON -m pip install rust/target/wheels/perspective_python*.whl -vv
+for %%w in (%CARGO_TARGET_DIR%\wheels\perspective_python*.whl) do "%PYTHON%" -m pip install "%WHEEL%" -vv
 if errorlevel 1 exit /b 1
 
 echo "They can build perspective. Why can't they make a cup of coffee that tastes good?"
